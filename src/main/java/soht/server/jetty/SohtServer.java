07d8f45a970bc7caa919ecf25398b181941a9895
Guy Driesen <guydriesen@users.noreply.github.com>
2016-09-13 23:04:45 +0200

    Named parameters for listen address, port & deployment path

--- src/main/java/soht/server/jetty/SohtServer.java
+++ src/main/java/soht/server/jetty/SohtServer.java
@@ -4,7 +4,6 @@ import java.net.URL;
 import java.security.ProtectionDomain;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.StringTokenizer;
 
 import org.eclipse.jetty.annotations.ServletContainerInitializersStarter;
 import org.eclipse.jetty.apache.jsp.JettyJasperInitializer;
@@ -12,39 +11,62 @@ import org.eclipse.jetty.plus.annotation.ContainerInitializer;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.webapp.WebAppContext;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+import lombok.Getter;
 
 public class SohtServer {
+
+	@Option(name = "-l", aliases = "--listen-addess", usage = "address to listen on")
+	@Getter
+	private String host = "ALL";
+
+	@Option(name = "-p", aliases = "--port", usage = "port to listen on", metaVar = "INT")
+	@Getter
+	private int port = 8080;
+
+	@Option(name = "-d", aliases = "--deploy-path", usage = "server deployment path")
+	@Getter
+	private String path = "/soht";
+
+	@Option(name = "-h", aliases = "--help", usage = "prints this message", help = true)
+	private void printHelp(boolean cleanExit) {
+		parser.printUsage(System.err);
+		System.exit(cleanExit ? 0 : 1);
+	}
+
+	CmdLineParser parser;
+
+	private void parseArguments(String[] args) {
+		parser = new CmdLineParser(this);
+		try {
+			parser.parseArgument(args);
+		} catch (CmdLineException e) {
+			System.err.println(e.getMessage());
+			printHelp(false);
+		}
+	}
 
 	public static void main(String[] args) throws Exception {
 		// Use the ecj compiler to compile the jsp's
 		System.setProperty("org.apache.jasper.compiler.disablejsr199", "false");
 
-		// Default local bind address & port
-		String host = "ALL"; // "localhost"
-		int port = 8080;
-
-		// Parse arguments
-		if (args.length != 0) {
-			StringTokenizer tokens = new StringTokenizer(args[0], ":");
-			if (tokens.countTokens() == 1) {
-				port = Integer.parseInt(tokens.nextToken());
-			} else {
-				host = tokens.nextToken();
-				port = Integer.parseInt(tokens.nextToken());
-			}
-		}
+		SohtServer ss = new SohtServer();
+		ss.parseArguments(args);
 
 		Server server = new Server();
 
 		ServerConnector serverConnector = new ServerConnector(server);
-		if (!host.equals("ALL")) {
-			serverConnector.setHost(host);
+		if (!ss.getHost().equals("ALL")) {
+			serverConnector.setHost(ss.getHost());
 		}
-		serverConnector.setPort(port);
+		serverConnector.setPort(ss.getPort());
 		server.addConnector(serverConnector);
 
 		WebAppContext webAppContext = new WebAppContext();
-		webAppContext.setContextPath("/");
+		webAppContext.setContextPath(ss.getPath());
 
 		// Initialize the jsp engine
 		JettyJasperInitializer sci = new JettyJasperInitializer();
